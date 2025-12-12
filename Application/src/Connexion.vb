336e50b3a5e7ed66f2a8dbab06834ce342796682
Quentin GUEZENOC <qguezenoc@estran-brest.education>
2025-12-12 09:46:54 +0100

    Pareil

--- Application/src/Connexion.vb
+++ Application/src/Connexion.vb
@@ -32,7 +32,7 @@ Public Class Connexion
             myCommandUtil.Parameters.AddWithValue(":mdp", TextBox_MDP.Text) ' Définition de :mdp ici (Text_MDP.Text est utilisé en tant que chaîne de caractère)
             myReaderUtil = myCommandUtil.ExecuteReader
 
-            ' Test si la combinaison matricule/mot de passe est trouvée, si oui, on cherche le rôle de l'utilisateur
+            ' Test si la combinaison matricule/mot de passe est trouvée, si oui, on récupère le rôle de l'utilisateur grâce à la fonction stockée getRole()
             If myReaderUtil.Read() Then
                 GlobalData.MatriculeUtilisateurConnecte = TextBox_Login.Text
                 ' Requête qui récupère le rôle de l'utilisateur grâce à la fonction stockée getRole()
@@ -63,44 +63,6 @@ Public Class Connexion
                     End If
                 End If
                 myReaderRole.Close() ' Fermeture du reader Rôle
-
-                '' Requête qui cherche si l'utilisateur est un visiteur
-                'Dim queryV As String = "SELECT matriculevisiteur
-                '                        FROM visiteur
-                '                        WHERE matriculevisiteur = '" & GlobalData.MatriculeUtilisateurConnecte & "';"
-                'myCommandV.Connection = myConnection
-                'myCommandV.CommandText = queryV
-                'myReaderV = myCommandV.ExecuteReader
-
-                'If myReaderV.Read() Then ' Positionne le curseur sur la ligne
-                '    ' OUVERTURE DE LA FENETRE VISITEUR
-                '    Dim f As New GestionCompte
-                '    f.Text = "Gestion des comptes-rendus de " & myReaderUtil.GetString(1) & " " & myReaderUtil.GetString(0)
-                '    f.Show()
-                'Else
-                '    ' Requête qui cherche si l'utilisateur est un délégué
-                '    Dim queryD As String = "SELECT matriculedelegue 
-                '                            FROM delegue
-                '                            WHERE delegue.matriculedelegue = '" & GlobalData.MatriculeUtilisateurConnecte & "';"
-                '    myCommandD.Connection = myConnection
-                '    myCommandD.CommandText = queryD
-                '    myReaderD = myCommandD.ExecuteReader
-
-                '    If myReaderD.Read() Then ' Positionne le curseur sur la ligne
-                '        ' OUVERTURE DE LA FENETRE DELEGUE
-                '        Dim f As New ConsulterActiviteEquipe
-                '        f.Text = "Activité de l'équipe de " & myReaderUtil.GetString(1) & " " & myReaderUtil.GetString(0)
-                '        f.MatriculeDelegue = GlobalData.MatriculeUtilisateurConnecte
-                '        f.Show()
-                '    Else
-                '        ' OUVERTURE DE LA FENETRE RESPONSABLE
-                '        Dim f As New ListeEquipe
-                '        f.Text = "Liste des équipes de " & myReaderUtil.GetString(1) & " " & myReaderUtil.GetString(0)
-                '        f.Show()
-                '    End If
-                '    myReaderD.Close() ' Fermeture du reader Délégué
-                'End If
-                'myReaderV.Close() ' Fermeture du reader Visiteur
             Else
                 MsgBox("Nom d'utilisateur ou mot de passe incorrect.")
             End If
